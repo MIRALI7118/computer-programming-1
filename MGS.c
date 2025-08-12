91eb05a6ef1d6d58cbe9f9c9306a5ccf0bf4365d
MIRALI7118 <miralipatel81@gmail.com>
2025-08-12 17:06:39 +0530

    Add files via upload

---
+++ MGS.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+printf("enter number\n");
+int number;
+scanf ("%d",&two number);
+if(number%2==0)
+{
+printf("number is even");
+}
+else
+{
+printf("number is odd");
+}
+return 0;
+}
+
+
+
+
+
+
+
